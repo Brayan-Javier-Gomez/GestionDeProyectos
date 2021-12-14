b25ac9bfbcdf401d004c1cb4513266766ffa85f7
Brayan-Javier-Gomez <brayanjaviergomez.g@gmail.com>
2021-12-13 22:40:58 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
+const cors = require("cors");
 port = process.env.PORT;
 
-
+app.use(cors())
+ 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 
